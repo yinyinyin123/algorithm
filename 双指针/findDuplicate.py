f0ca5fab152476dc720287b7100835e699c0fb66
yinyinyin123 <1350526138@qq.com>
2020-03-06 16:32:05 +0800

    leetcode 287 寻找重复数 快慢指针找环

--- 双指针/findDuplicate.py
+++ 双指针/findDuplicate.py
@@ -2,22 +2,23 @@
 ### one code one day
 ### 2020/03/06
 ### leetcode 287 寻找重复数
-### 二分法 时间复杂度O(nlgn)
+### 快慢指针 时间复杂度O(n)
 
 
 def findDuplicate(self, nums: List[int]) -> int:
-    def count_number(start,end):
-        count = 0
-        for num in nums:
-            if(start<=num<=end):
-                count += 1
-        return count
-    start = 1
-    end = len(nums) - 1
-    while(start < end):
-        mid = int((start + end) / 2)
-        if(count_number(start,mid) <= mid-start+1):
-            start = mid + 1
-        else:
-            end = mid 
-    return start
+    
+    slow, fast = 0, 0
+    while True:
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+
+        if slow == fast:
+            break
+        
+    find = 0
+    while True:
+        find = nums[find]
+        slow = nums[slow]
+        if find == slow:
+            return find
+
