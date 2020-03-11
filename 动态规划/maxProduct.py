aa5409636527fad109db9769ec9d76d9ea0fd514
yinyinyin123 <1350526138@qq.com>
2020-03-11 12:04:16 +0800

    leetcode 152 乘积最大子序列

---
+++ 动态规划/maxProduct.py
@@ -0,0 +1,28 @@
+
+### one code one day
+### 2020/03/11
+### leetcode 152 乘积最大子序列
+### 动态规划 实现的代码不是很简洁
+### 维护最大的正值和最小的负值
+
+def maxProduct(self, nums: List[int]) -> int:
+	### 最大正值，最小负值
+    positive = negative = None
+    res = -float('inf')
+    for num in nums:
+        if(num == 0):
+        	### 如果当前num为0，重置
+            res = max(0, res)
+            positive = negative = None
+        elif(num > 0):
+        	### 交换
+            positive, negative = positive * num if positive != None else num, negative * num if negative != None else None
+            res = max(positive, res)
+        else:
+        	### 交换
+            positive, negative= negative * num if negative != None else None, positive * num if positive != None else num
+            if(positive):
+                res = max(positive, res)
+            else:
+                res = max(negative, res)
+    return res
