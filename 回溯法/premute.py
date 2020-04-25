d84df8d762cafd9e317a9b714439f0b5a42764ed
yinyinyin123 <1350526138@qq.com>
2020-04-25 08:32:33 +0800

    leetcode 46 全排列

---
+++ 回溯法/premute.py
@@ -0,0 +1,28 @@
+
+### one code one day
+### 2020/04/25
+### leetcode 46 全排列
+### 回溯法
+
+class Solution:
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        def backtrack(first = 0):
+            # 所有数都填完了
+            if first == n:  
+                res.append(nums[:])
+            for i in range(first, n):
+                # 动态维护数组
+                nums[first], nums[i] = nums[i], nums[first]
+                # 继续递归填下一个数
+                backtrack(first + 1)
+                # 撤销操作
+                nums[first], nums[i] = nums[i], nums[first]
+
+        n = len(nums)
+        res = []
+        backtrack()
+        return res
